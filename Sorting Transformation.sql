a53186dd505551f05129a4767c98cac4070758bc
nasreen-S <nashu9003@gmail.com>
2022-09-27 17:53:49 +0530

    Sorting Transformation
    How sorting Transformation Takes place from source to destination

---
+++ Sorting Transformation.sql
@@ -0,0 +1,29 @@
+--------------------Sorting Transformation----------------
+
+create table empsql
+(
+id int,
+name varchar(20),
+salary float,
+age int,
+designation varchar(30)
+)
+
+insert into empsql values(1315,'Nasreen',25000,21,'Trainee'),
+(1315,'Nasreen',25000,21,'Trainee'),
+(1315,'Nasreen',25000,21,'Trainee'),
+(1045,'Fayaz',35000,20,'HR'),
+(1045,'Fayaz',35000,20,'HR'),
+(4567,'yasif',40000,25,'developer'),
+(4567,'yasif',40000,25,'developer'),
+(2314,'saniya',45000,27,'Business Analyst'),
+(2314,'saniya',45000,27,'Business Analyst'),
+(2314,'saniya',45000,27,'Business Analyst')
+
+select * from empsql
+
+select * from empsql_dst
+
+truncate table empsql_dst
+------------------------------presentation--------
+ 
