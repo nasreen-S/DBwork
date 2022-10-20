0272a0f6618b47da97e39ea73a33ef1eb94b8ec5
nasreen-S <nashu9003@gmail.com>
2022-10-20 14:34:13 +0530

    IICS_dst tables

---
+++ Codes in DBeaver/Dlithe_database_connection/IICS_dst tables.sql
@@ -0,0 +1,29 @@
+-------------IICS destination table-------
+
+select * from emp_NF_Dst
+
+select * from department
+
+select * from dep_nf_exp
+
+----------------SSRS------------
+
+create table customer_NF
+(
+
+)
+
+
+select * from customer_nf
+
+insert into customer_nf values(12,'nasreen',1234253401,'india'),(12,'nasreen',1234253401,'india'),
+(15,'muskan',1234253401,'india'),(12,'nasreen',1234253401,'india')
+
+select * from customer_nf_dst
+
+
+select * from join_nf
+
+
+
+
